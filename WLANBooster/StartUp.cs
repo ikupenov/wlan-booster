bd6d9d5f66b3672a3094610a704bc679eb061f35
ilievv <i.kupenov96@gmail.com>
2020-07-04 23:17:02 +0300

    fix

--- WLANBooster/StartUp.cs
+++ WLANBooster/StartUp.cs
@@ -11,19 +11,17 @@ namespace WLANBooster
         {
             Console.WriteLine("Select 0 to disable WLAN autoconfig or 1 to enable it:");
 
-            var command = string.Empty;
             int? option = null;
-
-            while (string.IsNullOrWhiteSpace(command) && !option.HasValue)
+            while (!option.HasValue)
             {
-                command = Console.ReadLine();
+                var command = Console.ReadLine();
                 if (int.TryParse(command, out var result))
                 {
                     option = result;
                 }
                 else
                 {
-                    Console.WriteLine("Invalid parameters. Try again.");
+                    Console.WriteLine("Invalid parameters, try again:");
                 }
             }
 
