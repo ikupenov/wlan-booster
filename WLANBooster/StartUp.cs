c194d2460e393e423b2254081f8fd9a76a6bc4b6
ilievv <i.kupenov96@gmail.com>
2020-07-04 23:13:44 +0300

    init

---
+++ WLANBooster/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+
+using NativeWifi;
+
+namespace WLANBooster
+{
+    public class StartUp
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Select 0 to disable WLAN autoconfig or 1 to enable it:");
+
+            var command = string.Empty;
+            int? option = null;
+
+            while (string.IsNullOrWhiteSpace(command) && !option.HasValue)
+            {
+                command = Console.ReadLine();
+                if (int.TryParse(command, out var result))
+                {
+                    option = result;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid parameters. Try again.");
+                }
+            }
+
+            var wlanClient = new WlanClient();
+
+            foreach (var client in wlanClient.Interfaces)
+            {
+                if (option == 0)
+                {
+                    var cmdCommand = $@"/C netsh wlan set autoconfig enabled=no interface=""{client.InterfaceName}""";
+                    Process.Start("CMD.exe", cmdCommand);
+                }
+                else if (option == 1)
+                {
+                    var cmdCommand = $@"/C netsh wlan set autoconfig enabled=yes interface=""{client.InterfaceName}""";
+                    Process.Start("CMD.exe", cmdCommand);
+                }
+            }
+        }
+    }
+}
